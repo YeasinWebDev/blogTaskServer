9eea1054397a52c374980c2247c588873a89c073
Yeasin <ar9485727@gmail.com>
2024-10-09 20:40:15 +0600

    add origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://blogs-task-client.vercel.app"],
+    origin: ["http://localhost:5173", "https://blogs-task-client.vercel.app", "https://blog-task-server.vercel.app"],
     credentials: true,
   })
 );
